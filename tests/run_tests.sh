0ac91613b81e369550c1b4212d20f29da1b44c2d
Michał Bachowski <michal.bachowski@tesco.com>
2024-10-15 16:27:25 +0200

    Add tests for fast exit

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -5,6 +5,26 @@ prefix="./fixtures"
 source "../progress_indicator.sh"
 setup_progress_indicator ./test-output
 
+########################
+# Fast exit
+echo ""
+echo "Fast Exit Test"
+echo "--------------"
+echo ""
+indicate_progress "a" "Running a" "${prefix}/fast.sh"
+
+start_progress_indicator
+
+echo ""
+echo "==================="
+########################
+
+########################
+# Complex dependencies
+echo ""
+echo "Complex Dependencies Test"
+echo "--------------------------"
+echo ""
 indicate_progress "a" "Running a" "${prefix}/a.sh"
 indicate_progress b "Installing b" "${prefix}/b.sh" a
 indicate_progress c "Updating c" "${prefix}/c.sh" "a b"
@@ -16,3 +36,8 @@ indicate_progress h "Running h" "${prefix}/b.sh"
 indicate_progress i "Running i" "${prefix}/c.sh"
 
 start_progress_indicator
+echo ""
+echo "==================="
+########################
+
+echo ""
